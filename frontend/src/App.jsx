016cb874adb4842ea76e7b34d701f35f60826703
sxrx99 <ms_hasnaoui@esi.dz>
2025-07-29 12:00:48 +0100

    setup ready

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+ function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <div className="bg-red-600 text-white p-10 rounded-xl shadow-lg text-xl font-bold">
+        Tailwind is working!
+      </div>
+    </div>
+  )
+}
+
+export default App ; 
